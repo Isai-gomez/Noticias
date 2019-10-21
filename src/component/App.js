e6f540dbae4fd664e42dc0f1a334aa50e752f033
Chai <isaigomezhernandez.com>
2019-10-21 17:22:19 -0500

    Guardando la noticias en el State

--- src/component/App.js
+++ src/component/App.js
@@ -1,12 +1,36 @@
-import React from "react";
+import React, { Component } from "react";
 import Header from "./Header";
+import { ReplaySubject } from "rxjs";
 
-function App() {
-  return (
-    <div className="App">
-      <Header titulo={"Noticias"}></Header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      noticias: []
+    };
+  }
+  componentDidMount() {
+    this.consutaNoticia();
+  }
+  consutaNoticia = () => {
+    let url =
+      "https://newsapi.org/v2/top-headlines?country=mx&category=general&apiKey=127b7ec038b4407b954a1f5606cf9f70";
+    fetch(url)
+      .then(respuesta => {
+        return respuesta.json();
+      })
+      .then(noticias => {
+        this.setState({
+          noticias: noticias.articles
+        });
+      });
+  };
+  render() {
+    return (
+      <div className="App">
+        <Header titulo={"Noticias"}></Header>
+      </div>
+    );
+  }
 }
-
 export default App;
