3cc8f9bba3df2cb79a98e9d5c32567c008c053db
Chai <isaigomezhernandez.com>
2019-10-22 02:23:23 -0500

    Mostrando las noticias

--- src/component/Noticia.js
+++ src/component/Noticia.js
@@ -1,10 +1,28 @@
 import React from "react";
 const Noticia = props => {
   const { urlToImage, url, title, description, source } = props.noticia;
-  console.log(urlToImage);
+
   return (
-    <div>
-      <p>Desde noticia</p>
+    <div className="row s10 m6 l10">
+      <div className="card">
+        <div className="card-image">
+          <img src={urlToImage} alt={title} />
+          <span className="card-title">{source.name}</span>
+        </div>
+        <div className="card-content">
+          <h3>{title}</h3>
+          <p>{description}</p>
+        </div>
+        <div className="card-action">
+          <a
+            href={url}
+            target="_blank"
+            className="waves-effects waves-light btn"
+          >
+            Leer
+          </a>
+        </div>
+      </div>
     </div>
   );
 };
